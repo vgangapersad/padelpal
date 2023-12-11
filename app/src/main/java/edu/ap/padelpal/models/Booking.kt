c93722a5721d4053a4a26a59da060a892dacff10
Viral Ganga Persad <viral.ganga@gmail.com>
2023-12-11 11:20:28 +0100

    Created model for Bookings and clubs

--- app/src/main/java/edu/ap/padelpal/models/Booking.kt
+++ app/src/main/java/edu/ap/padelpal/models/Booking.kt
@@ -3,7 +3,6 @@ package edu.ap.padelpal.models
 import java.util.Date
 
 data class Booking(
-    val id: String,
     val clubId: String,
     val matchId: String,
     val userId: String,
--- app/src/main/java/edu/ap/padelpal/models/Club.kt
+++ app/src/main/java/edu/ap/padelpal/models/Club.kt
@@ -1,22 +1,21 @@
 package edu.ap.padelpal.models
 
 data class Club(
-    val id: String,
-    val name: String,
-    val location: Location,
-    val imageUrl: String,
-    val openingHours: OpeningHours
+    val name: String = "",
+    val location: Location = Location(),
+    val imageUrl: String = "",
+    val openingHours: OpeningHours = OpeningHours()
 )
 
 data class Location(
-    val latitude: Double,
-    val longitude: Double,
-    val country: String,
-    val city: String,
-    val address: String
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val country: String = "",
+    val city: String = "",
+    val address: String = ""
 )
 
 data class OpeningHours(
-    val startTime: Int,
-    val endTime: Int
+    val startTime: Int = 0,
+    val endTime: Int = 0
 )
