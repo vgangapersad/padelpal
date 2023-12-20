861728b211b10e9ba0f5bed2a0cee6dc838a7b1c
RUQAYA\ruqay <ruqaya.tamimi@hotmail.com>
2023-12-20 23:56:45 +0100

    modify settings screen

--- app/src/main/java/edu/ap/padelpal/ui/profile/SettingsFragment.kt
+++ app/src/main/java/edu/ap/padelpal/ui/profile/SettingsFragment.kt
@@ -2,12 +2,11 @@ package edu.ap.padelpal.ui.profile
 
 import android.content.Context
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.LocationOn
@@ -16,12 +15,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -33,12 +35,11 @@ import edu.ap.padelpal.models.CourtPositionPreference
 import edu.ap.padelpal.models.HandPreference
 import edu.ap.padelpal.models.LocationPreference
 import edu.ap.padelpal.models.MatchTypePreference
-import edu.ap.padelpal.models.Preferences
 import edu.ap.padelpal.models.TimePreference
-import edu.ap.padelpal.models.User
 import edu.ap.padelpal.presentation.sign_in.UserData
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import okhttp3.internal.wait
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,19 +49,30 @@ fun SettingsScreen(
     navController: NavController,
     onSignOut: () -> Unit
 ) {
-   Scaffold(
+
+    val snackbarHostState = remember { SnackbarHostState() }
+
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
         topBar = {
-            SmallTopAppBar(
-                title = { Text("Settings") },
-                navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "Back"
-                        )
-                    }
-                },
-                actions = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.background,
+                        titleContentColor = MaterialTheme.colorScheme.onBackground,
+                    ),
+                    title = { Text("Settings") },
+                    navigationIcon = {
+                        IconButton(onClick = { navController.popBackStack() }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBack,
+                                contentDescription = "Back"
+                            )
+                        }
+                    },
+                    actions = {
                     IconButton(onClick = onSignOut) {
                         Icon(
                             painter = painterResource(id = R.drawable.logout),
@@ -68,140 +80,107 @@ fun SettingsScreen(
                         )
                     }
                 }
-            )
-        },
-        content = { innerPadding ->
+                )
+        }
+    ) { innerPadding ->
+        Box( modifier = Modifier
+            .padding(innerPadding)
+            .padding(16.dp)) {
             SettingsContent(
-                modifier = Modifier.padding(innerPadding),
                 userData = userData,
             )
+
         }
-    )
+    }
 }
 
-
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SettingsContent(
-    modifier: Modifier = Modifier,
-    userData: UserData?) {
-
+fun SettingsContent(userData: UserData?) {
     var usernameState by remember { mutableStateOf(userData?.username ?: "") }
     val coroutineScope = rememberCoroutineScope()
     val userRepository = UserRepository()
+    var selectedLocation by remember { mutableStateOf(LocationPreference.antwerpen)}
+
+
     // Fetch the display name from Firestore when the screen is created
     LaunchedEffect(key1 = userData?.userId) {
         if (userData != null) {
             coroutineScope.launch {
                 val user = userRepository.getUserFromFirestore(userData.userId)
                 usernameState = user?.displayName ?: ""
+                selectedLocation = user?.preferences?.location!!
             }
         }
     }
-    val locations = listOf(
-        "Antwerpen",
-        "Gent",
-        "Brussel",
-        "Mechelen",
-        "Not set"
-    )
-    val bestHand = listOf(
-        "Right-handed",
-        "Left-handed",
-        "Both hands",
-        "Not set"
-    )
-    val courtPosition = listOf(
-        "Both sides",
-        "Backhand",
-        "Forehand",
-        "Not set"
-    )
-    val matchType = listOf(
-        "Competitive",
-        "Friendly",
-        "Both",
-        "Not set"
-    )
-    val preferredTime = listOf(
-        "Morning",
-        "Afternoon",
-        "Evening",
-        "All day",
-        "Not set"
-    )
 
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
+    var selectedBestHand by remember { mutableStateOf(HandPreference.leftHanded)}
+    var selectedCourtPosition by remember { mutableStateOf(CourtPositionPreference.bothSides)}
+    var selectedMatchType by remember { mutableStateOf(MatchTypePreference.friendly)}
+    var selectedPreferredTime by remember { mutableStateOf(TimePreference.morning)}
+    var isFocused by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-        ProfilePicture(userData)
 
-        Column(
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
-        )
-        {
-                OutlinedTextField(
-                    value = usernameState,
-                    onValueChange = {usernameState = it},
-                    label = { Text("Display Name") },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(bottom = 15.dp)
-                )
-            Selection(
-                selectionData = locations,
-                selectionLabel = "Location",
-                icon = Icons.Filled.LocationOn,
-                iconResource = null
+
+    LazyColumn {
+        item {
+            ProfilePicture(userData)
+            OutlinedTextField(
+                value = if (usernameState != null) usernameState.toString() else "",
+                onValueChange = { usernameState = it },
+                label = { Text("Display Name") },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                    autoCorrect = true,
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 15.dp)
             )
-            Selection(
-                selectionData = bestHand,
-                selectionLabel = "Best hand",
-                icon = null,
-                iconResource = painterResource(id = R.drawable.hand)
-            )
-            Selection(
-                selectionData = courtPosition,
-                selectionLabel = "Court position",
-                icon = null,
-                iconResource = painterResource(id = R.drawable.position)
-            )
-            Selection(
-                selectionData = matchType,
-                selectionLabel = "Match type",
-                icon = null,
-                iconResource = painterResource(id = R.drawable.padel)
-            )
-            Selection(
-                selectionData = preferredTime,
-                selectionLabel = "Preferred time to play",
-                icon = null,
-                iconResource = painterResource(id = R.drawable.sun)
-            )
-            Spacer(modifier = Modifier.height(80.dp))
         }
-        val context = LocalContext.current
-        Button(
-            onClick = {
-                if (userData != null) {
-                    coroutineScope.launch {
-                        saveDisplayNameToFirestore(userData.userId, usernameState, context)
 
+        item {
+
+            Selection(
+                label = "Location",
+                selected = selectedLocation,
+                onSelectedChanged = { newValue ->
+                    selectedLocation = newValue
+                },
+                enumValues = LocationPreference.values(),
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+        }
+        item {
+            val context = LocalContext.current
+            Button(
+                onClick = {
+                    if (userData != null) {
+                        coroutineScope.launch {
+                            saveDisplayNameToFirestore(userData.userId, usernameState, context)
+                            saveSelectionsToFirestore(
+                                userData.userId,
+                                selectedLocation,
+                                context)
+                        }
                     }
-                }
-            },
-            modifier = Modifier
-                .align(Alignment.End)
-                .padding(top = 16.dp)
-        ) {
-            Text("Save")
+                },
+                modifier = Modifier
+                    .clip(RoundedCornerShape(50))
+                    .height(48.dp)
+                    .fillMaxSize(),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+            ) {
+                Text("Save")
+            }
+            Spacer(modifier = Modifier.height(100.dp))
         }
     }
 }
+
 
 // Function to save the display name to Firestore
 private suspend fun saveDisplayNameToFirestore(uid: String, displayName: String, context: Context) {
@@ -214,6 +193,22 @@ private suspend fun saveDisplayNameToFirestore(uid: String, displayName: String,
         // Handle success if needed
     } catch (e: Exception) {
         // Handle exception
+    }
+}
+private suspend fun saveSelectionsToFirestore(
+    uid: String,
+    selectedLocation: LocationPreference,
+    context: Context
+) {
+    try {
+        val db = Firebase.firestore
+        val userDocument = db.collection("users").document(uid)
+
+        userDocument.update(
+            "preferences.location", selectedLocation).await()
+        Toast.makeText(context, "Preferences Saved", Toast.LENGTH_SHORT).show()
+
+    } catch (e: Exception) {
     }
 }
 
@@ -238,77 +233,148 @@ fun ProfilePicture(userData: UserData?) {
     }
 }
 
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Selection(
-    selectionData: List<String>,
-    selectionLabel: String,
-    icon: ImageVector?,
-    iconResource: Painter?
+fun <T: Enum<T>> Selection(
+    label: String,
+    selected: T,
+    onSelectedChanged: (T) -> Unit,
+    enumValues: Array<T>,
+    icon: ImageVector? = null,
+    iconResource: Painter? = null
 ) {
+    var isFocused by remember { mutableStateOf(false) }
     var expanded by remember { mutableStateOf(false) }
-    var textFieldValue by remember { mutableStateOf("") }
 
     ExposedDropdownMenuBox(
         expanded = expanded,
-        onExpandedChange = {
-            expanded = !expanded
-        },
+        onExpandedChange = { expanded = !expanded },
         modifier = Modifier
-            .padding(bottom = 15.dp)
-    ) {
+            .border(
+                1.dp,
+                if (isFocused) MaterialTheme.colorScheme.primary else Color.Gray,
+                RoundedCornerShape(4.dp)
+            )) {
         TextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.Transparent)
-                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
-                .menuAnchor(),
-            value = textFieldValue,
-            onValueChange = { newValue ->
-                textFieldValue = newValue
-            },
-            label = { Text(selectionLabel) },
+                .menuAnchor()
+                .onFocusChanged { focusState -> isFocused = focusState.isFocused }
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp)),
+            readOnly = true,
+            value = selected.name,
+            onValueChange = {},
+            label = { Text(label) },
             trailingIcon = {
                 if (icon != null) {
                     Icon(
                         imageVector = icon,
-                        contentDescription = selectionLabel,
+                        contentDescription = label,
                         tint = MaterialTheme.colorScheme.primary
                     )
                 }
                 if (iconResource != null) {
                     Icon(
                         painter = iconResource,
-                        contentDescription = selectionLabel,
+                        contentDescription = label,
                         tint = MaterialTheme.colorScheme.primary
                     )
                 }
             },
             colors = TextFieldDefaults.textFieldColors(
-                containerColor = Color.Transparent
+                containerColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
             ),
         )
-
-        // filter options based on text field value
-        val filteringOptions = selectionData.filter { it.contains(textFieldValue, ignoreCase = true) }
-        if (filteringOptions.isNotEmpty()) {
-            ExposedDropdownMenu(
-                expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                filteringOptions.forEach { selectedData ->
-                    DropdownMenuItem(
-                        text = { Text(selectedData) },
-                        onClick = {
-                            textFieldValue = selectedData
-                            expanded = false
-                        },
-                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
-                    )
-                }
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+        ) {
+            enumValues.forEach { preference  ->
+                DropdownMenuItem(
+                    text = { Text(preference.name.capitalize()) },
+                    onClick = {
+                        onSelectedChanged(preference)
+                        expanded = false
+                    },
+                )
             }
         }
     }
+
 }
+
+//
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun <T : Enum<T>> Selection(
+//    label: String,
+//    selected: T,
+//    enumValues: Array<T>,
+//    icon: ImageVector?,
+//    iconResource: Painter?,
+//    getName: @Composable (T) -> String
+//){
+//    var isFocused by remember { mutableStateOf(false) }
+//    var expanded by remember { mutableStateOf(false) }
+//    var selectedPreference by remember { mutableStateOf(selected) }
+//
+//    ExposedDropdownMenuBox(
+//        expanded = expanded,
+//        onExpandedChange = { expanded = !expanded },
+//        modifier = Modifier
+//            .border(
+//                1.dp,
+//                if (isFocused) MaterialTheme.colorScheme.primary else Color.Gray,
+//                RoundedCornerShape(4.dp)
+//            )
+//    ) {
+//        TextField(
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .menuAnchor()
+//                .onFocusChanged { focusState -> isFocused = focusState.isFocused }
+//                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp)),
+//            readOnly = true,
+//            value = getName(selected),
+//            onValueChange = {},
+//            label = { Text(label) },
+//            trailingIcon = {
+//                if (icon != null) {
+//                    Icon(
+//                        imageVector = icon,
+//                        contentDescription = label,
+//                        tint = MaterialTheme.colorScheme.primary
+//                    )
+//                } else if (iconResource != null) {
+//                    Icon(
+//                        painter = iconResource,
+//                        contentDescription = label,
+//                        tint = MaterialTheme.colorScheme.primary
+//                    )
+//                }
+//            },
+//            colors = TextFieldDefaults.textFieldColors(
+//                containerColor = Color.Transparent,
+//                unfocusedIndicatorColor = Color.Transparent,
+//                focusedIndicatorColor = Color.Transparent
+//            )
+//        )
+//
+//        ExposedDropdownMenu(
+//            expanded = expanded,
+//            onDismissRequest = { expanded = false },
+//        ) {
+//            enumValues.forEach { preference ->
+//                DropdownMenuItem(
+//                    text = { Text(getName(preference)) },
+//                    onClick = {
+//                        selectedPreference = preference
+//                        expanded = false
+//                    }
+//                )
+//            }
+//        }
+//    }
+//}
