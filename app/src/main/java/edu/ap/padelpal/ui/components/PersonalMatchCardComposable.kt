0d24d109377d241ee7f21b7f521503ce7a4967db
Viral Ganga Persad <viral.ganga@gmail.com>
2023-12-20 19:09:03 +0100

    Add personal matches to a profile and created necessary components

---
+++ app/src/main/java/edu/ap/padelpal/ui/components/PersonalMatchCardComposable.kt
@@ -0,0 +1,193 @@
+package edu.ap.padelpal.ui.components
+
+import android.os.Build
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import edu.ap.padelpal.models.MatchDetailsResponse
+import edu.ap.padelpal.presentation.sign_in.UserData
+import edu.ap.padelpal.utilities.MatchUtils
+import edu.ap.padelpal.utilities.formatDateForDisplay
+import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.LocalTime
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun PersonalMatchCard(user: UserData, match: MatchDetailsResponse) {
+    val matchUtils = MatchUtils()
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val isOrganizer = match.match.organizerId == user.userId
+    var isJoined by remember { mutableStateOf(match.match.playerIds.contains(user.userId)) }
+    var joinedPlayers by remember { mutableStateOf(match.match.playerIds.size) }
+    var buttonText = "Join"
+
+    if (isOrganizer) {
+        buttonText = "Organized"
+    } else {
+        if (isJoined) {
+            buttonText = "Leave"
+        }
+    }
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, bottom = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+    ) {
+        Box(modifier = Modifier.height(220.dp)) {
+            AsyncImage(
+                model = match.club.imageUrl,
+                contentDescription = match.match.title,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .blur(
+                        radiusX = 4.dp,
+                        radiusY = 4.dp,
+                        edgeTreatment = BlurredEdgeTreatment(RoundedCornerShape(8.dp))
+                    ),
+
+                contentScale = ContentScale.Crop
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.surfaceDim.copy(alpha = 0.4f))
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = match.match.title,
+                    style = MaterialTheme.typography.headlineSmall.copy(color = Color.White),
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row {
+                    InformationChip("${joinedPlayers}/${match.match.amountOfPlayers}")
+                    Spacer(modifier = Modifier.width(8.dp))
+                    InformationChip(match.match.matchType.name.capitalize(Locale("EN")))
+                    Spacer(modifier = Modifier.width(8.dp))
+                    InformationChip(match.match.genderPreference.name.capitalize(Locale("EN")))
+                }
+                Spacer(modifier = Modifier.height(25.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val startTime = LocalTime.ofSecondOfDay(match.booking.startTime)
+                    val timeslot = "${startTime} - ${startTime.plusMinutes(match.booking.durationMinutes.toLong())}"
+                    Icon(Icons.Default.DateRange, contentDescription = "Date", tint = Color.White, modifier = Modifier.size(18.dp))
+                    Spacer(modifier = Modifier.width(15.dp))
+                    Text(text = "${formatDateForDisplay(LocalDate.ofEpochDay(match.booking.date))}  $timeslot", style = MaterialTheme.typography.bodyMedium, color = Color.White)
+                }
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(Icons.Default.LocationOn, contentDescription = "Location", tint = Color.White, modifier = Modifier.size(18.dp))
+                    Spacer(modifier = Modifier.width(15.dp))
+                    Text(text = "${match.club.name} (${match.club.location.city})", style = MaterialTheme.typography.bodyMedium, color = Color.White)
+                }
+
+                Button(
+                    onClick = {
+                        if(isOrganizer){
+                            Toast.makeText(context, "You organized this match", Toast.LENGTH_LONG).show()
+                        } else {
+                            if (isJoined) {
+                                val result = scope.launch {
+                                    matchUtils.removePlayerByMatchId(match.match.id, user.userId)
+                                }
+                                if (!result.isCancelled) {
+                                    isJoined = false
+                                    joinedPlayers -= 1
+                                    Toast.makeText(
+                                        context,
+                                        "You left ${match.match.title}",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                } else {
+                                    Toast.makeText(context, "Try again later", Toast.LENGTH_LONG)
+                                        .show()
+                                }
+                            } else {
+                                val result = scope.launch {
+                                    matchUtils.addPlayerByMatchId(match.match.id, user.userId)
+                                }
+                                if (!result.isCancelled) {
+                                    isJoined = true
+                                    joinedPlayers += 1
+                                    Toast.makeText(
+                                        context,
+                                        "You joined ${match.match.title}",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                } else {
+                                    Toast.makeText(context, "Try again later", Toast.LENGTH_LONG)
+                                        .show()
+                                }
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .clip(RoundedCornerShape(50)),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)){
+                    Text(
+                        text = buttonText,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/edu/ap/padelpal/ui/profile/ProfileFragment.kt
+++ app/src/main/java/edu/ap/padelpal/ui/profile/ProfileFragment.kt
@@ -1,24 +1,34 @@
 package edu.ap.padelpal.ui.profile
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -29,151 +39,229 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
 import edu.ap.padelpal.data.firestore.UserRepository
+import edu.ap.padelpal.models.MatchDetailsResponse
 import edu.ap.padelpal.models.User
 import edu.ap.padelpal.presentation.sign_in.UserData
+import edu.ap.padelpal.ui.components.IndeterminateCircularIndicator
+import edu.ap.padelpal.ui.components.PersonalMatchCard
+import edu.ap.padelpal.utilities.MatchUtils
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-
+@RequiresApi(Build.VERSION_CODES.O)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     userData: UserData?,
     navController: NavController
-    ) {
-
+) {
     val coroutineScope = rememberCoroutineScope()
     val userRepository = UserRepository()
+    val matchUtils = MatchUtils()
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
     var user by remember(userData?.userId) {
         mutableStateOf<User?>(null)
     }
 
-    // Fetch the user data when the screen is created
+    var matches by remember {
+        mutableStateOf<List<MatchDetailsResponse>>(emptyList())
+    }
+
+    var done by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = done) {
+        delay(5000)
+        done = true
+    }
+
     LaunchedEffect(key1 = userData?.userId) {
         if (userData != null) {
             coroutineScope.launch {
                 val fetchedUser = userRepository.getUserFromFirestore(userData.userId)
-                // Update the user data using the remember variable
                 user = fetchedUser
+            }
+            matches = matchUtils.getJoinedMatchesWithDetailsByUser(userData.userId)
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onBackground,
+                ),
+                title = {
+                    Text(
+                        "Profile",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { navController.navigate("Settings") }) {
+                        Icon(
+                            imageVector = Icons.Rounded.Settings,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp)
+                .fillMaxSize()
+        ) {
+            item {
+                ProfileInformation(userData = userData, user = user, navController = navController)
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+            if (matches.isNotEmpty()) {
+                items(matches) { match ->
+                    if (userData != null) {
+                        PersonalMatchCard(userData, match)
+                    }
+                }
+            } else {
+                item {
+                    if (done) {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            Row(modifier = Modifier.align(Alignment.Center)) {
+                                Text(text = "Nothing here yet")
+                            }
+                        }
+                    } else {
+                        IndeterminateCircularIndicator(label = "Loading ${user?.displayName}'s matches")
+                    }
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.height(100.dp))
             }
         }
     }
-    Surface(
-        modifier = Modifier.padding(10.dp),
+}
+
+@Composable
+fun ProfileInformation(userData: UserData?, user: User?, navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
     ) {
-        Column {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                if (userData?.profilePictureUrl != null) {
-                    AsyncImage(
-                        model = userData.profilePictureUrl,
-                        contentDescription = "Profile Picture",
-                        modifier = Modifier
-                            .size(72.dp)
-                            .clip(CircleShape),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-                Spacer(modifier = Modifier.width(16.dp))
-                Column(
-                    modifier = Modifier.weight(1f)
-                ) {
-                    if (userData?.username != null) {
-                        user?.let {
-                            Text(
-                                text = it.displayName,
-                                style = MaterialTheme.typography.headlineSmall,
-                                color = MaterialTheme.colorScheme.onBackground,
-                            )
-                        }
-
-                    }
-                    Text(
-                        "Antwerpen - België", color = MaterialTheme.colorScheme.secondary)
-                }
-
-                IconButton(onClick = {
-                    navController.navigate("Settings")
-                }) {
-                    Icon(
-                        imageVector = Icons.Rounded.Settings,
-                        contentDescription = "settings",
-                    )
-                }
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (userData?.profilePictureUrl != null) {
+                AsyncImage(
+                    model = userData.profilePictureUrl,
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(72.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
             }
-            // Green bar section
-            Spacer(modifier = Modifier.height(5.dp))
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(50))
-                    .background(MaterialTheme.colorScheme.secondaryContainer)
-                    .padding(horizontal = 80.dp, vertical = 10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                if (userData?.username != null) {
                     user?.let {
                         Text(
-                            text = it.level.toString(),
-                            color = MaterialTheme.colorScheme.onSecondaryContainer,
-                            style = MaterialTheme.typography.bodyLarge,
-
-                            )
-                    }
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "Level",
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    user?.let {
-                        Text(
-                            text = it.matchesPlayed.toString(),
-                            color = MaterialTheme.colorScheme.onSecondaryContainer,
-                            style = MaterialTheme.typography.bodyLarge,
-
-                            )
-                    }
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "Played",
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
-                }
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    user?.let {
-                        Text(
-                            text = it.matchesWon.toString(),
-                            color = MaterialTheme.colorScheme.onSecondaryContainer,
-                            style = MaterialTheme.typography.bodyLarge
+                            text = it.displayName,
+                            style = MaterialTheme.typography.headlineSmall,
+                            color = MaterialTheme.colorScheme.onBackground,
                         )
                     }
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "Wins",
-                        color = MaterialTheme.colorScheme.onSecondaryContainer,
-                        style = MaterialTheme.typography.bodyMedium
-                    )
                 }
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    "Antwerpen - België",
+                    color = MaterialTheme.colorScheme.secondary
+                )
             }
+        }
+    }
+
+    // Green bar section
+    Spacer(modifier = Modifier.height(15.dp))
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(50))
+            .background(MaterialTheme.colorScheme.secondaryContainer)
+            .padding(horizontal = 80.dp, vertical = 10.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            user?.let {
+                Text(
+                    text = it.level.toString(),
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    style = MaterialTheme.typography.bodyLarge,
+
+                    )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Level",
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            user?.let {
+                Text(
+                    text = it.matchesPlayed.toString(),
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    style = MaterialTheme.typography.bodyLarge,
+
+                    )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Played",
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            user?.let {
+                Text(
+                    text = it.matchesWon.toString(),
+                    color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "Wins",
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
